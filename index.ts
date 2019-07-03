77413ebf4d3398291bda84bf074db871fea38a50
Simon Mika <simon@mika.se>
2019-07-03 13:27:39 +0200

    Imported from Smoothly.

--- index.ts
+++ index.ts
@@ -1,17 +1,27 @@
 import { Address } from "./Address"
 import { Addresses } from "./Addresses"
+import { Color } from "./Color"
 import { Contact } from "./Contact"
 import { EmailAddresses } from "./EmailAddresses"
+import { Expand } from "./Expand"
+import { Fill } from "./Fill"
 import { IdentityNumber } from "./IdentityNumber"
+import { Message } from "./Message"
 import { Name } from "./Name"
 import { PhoneNumbers } from "./PhoneNumbers"
+import { Trigger } from "./Trigger"
 
 export {
 	Address,
 	Addresses,
-	Contact as Customer,
+	Color,
+	Contact,
 	EmailAddresses,
+	Expand,
+	Fill,
 	IdentityNumber,
+	Message,
 	Name,
 	PhoneNumbers,
+	Trigger,
 }
