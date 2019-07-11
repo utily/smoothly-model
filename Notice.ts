6e2348466ba1cf33739b59e10c907f4ea8b42861
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-07-11 10:11:34 +0200

    Added paranthesis

--- Notice.ts
+++ Notice.ts
@@ -5,7 +5,7 @@ export interface Notice {
 export namespace Notice {
 	export function is(value: any | Notice): value is Notice {
 		return typeof(value) == "object" &&
-			value.type == "success" || value.type == "warning" || value.type == "danger" || value.type == "default" &&
+			(value.type == "success" || value.type == "warning" || value.type == "danger" || value.type == "default") &&
 			typeof(value.message) == "string"
 	}
 }
