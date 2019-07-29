633f5068928a72f5552fa51eed90d16422c3b2cf
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-07-29 13:35:07 +0200

    valueCache can be null

--- ClientIdentifier.ts
+++ ClientIdentifier.ts
@@ -2,7 +2,7 @@ import { Identifier } from "authly"
 
 export abstract class ClientIdentifier {
 	private constructor() {}
-	private static valueCache: string
+	private static valueCache: string | null
 	static get value(): string {
 		ClientIdentifier.valueCache = localStorage.getItem("clientIdentifier")
 		if (!ClientIdentifier.valueCache) {
