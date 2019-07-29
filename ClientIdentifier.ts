515d7444f54ef91241b25a1a6b6c6de6f15dd132
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-07-29 13:13:38 +0200

    valueCache

--- ClientIdentifier.ts
+++ ClientIdentifier.ts
@@ -2,13 +2,13 @@ import { Identifier } from "authly"
 
 export abstract class ClientIdentifier {
 	private constructor() {}
-	private static value: string
-	get value(): string {
-		ClientIdentifier.value = localStorage.getItem("clientIdentifier")
-		if (!ClientIdentifier.value) {
-			ClientIdentifier.value = Identifier.generate(10)
-			localStorage.setItem("clientIdentifier", ClientIdentifier.value)
+	private static valueCache: string
+	static get value(): string {
+		ClientIdentifier.valueCache = localStorage.getItem("clientIdentifier")
+		if (!ClientIdentifier.valueCache) {
+			ClientIdentifier.valueCache = Identifier.generate(10)
+			localStorage.setItem("clientIdentifier", ClientIdentifier.valueCache)
 		}
-		return ClientIdentifier.value
+		return ClientIdentifier.valueCache
 	}
 }
