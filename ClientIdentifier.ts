ce3c980c886514ebc3d871cd4c45c7a19d7c2ba5
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-07-29 11:24:54 +0200

    Reworked ClientIdentifier

--- ClientIdentifier.ts
+++ ClientIdentifier.ts
@@ -1,11 +1,16 @@
 import { Identifier } from "authly"
 
-export namespace ClientIdentifier {
-
-	export function getClientId() {
-		let clientId = localStorage.getItem("clientId")
-		if (!clientId)
-			clientId = Identifier.generate(10)
-		return clientId
+export abstract class ClientIdentifier {
+	private constructor() {}
+	static set id(id: string) {
+		ClientIdentifier.id = id
+	}
+	static get id(): string {
+		ClientIdentifier.id = localStorage.getItem("clientId")
+		if (!ClientIdentifier.id) {
+			ClientIdentifier.id = Identifier.generate(10)
+			localStorage.setItem("clientId", ClientIdentifier.id)
+		}
+		return ClientIdentifier.id
 	}
 }
