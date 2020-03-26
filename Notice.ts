4fc5f570e566776d0127c1017c4c24d0f524cb3b
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2020-03-26 13:11:21 +0100

    Notice timer changes (#12)
    * Ability to change or disable timer for disappering
    
    * name change

--- Notice.ts
+++ Notice.ts
@@ -1,11 +1,15 @@
 export interface Notice {
 	type: "success" | "warning" | "danger" | "default"
 	message: string
+	remain?: true
+	timeout?: number
 }
 export namespace Notice {
 	export function is(value: any | Notice): value is Notice {
 		return typeof(value) == "object" &&
 			(value.type == "success" || value.type == "warning" || value.type == "danger" || value.type == "default") &&
-			typeof(value.message) == "string"
+			typeof(value.message) == "string" &&
+			(value.remain == undefined || value.remain == true) &&
+			(value.timeout == undefined || typeof(value.timeout) == "number")
 	}
 }
